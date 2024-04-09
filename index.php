ab0bba48f68f75af921f04ad42c2699302b6c555
Brian <isic19.esandovalf@itesco.edu.mx>
2024-04-09 13:42:00 -0600

    Mi primera pagina

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@2.11.6/dist/umd/popper.min.js" integrity="sha384-oNUfhFpLQzWSiK0v7yoWsbwYAy6AmU1s/Ut1BTJzDKlSRzjHK9/7imVRVNOOYi/u" crossorigin="anonymous"></script>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <title>PROYECTO</title>
+    <style>
+      body {
+            background-image: url('https://smartgs.com.mx/wp-content/uploads/2021/05/Cumplimiento-de-la-LGA-1080x675.jpg'); /* Cambia la ruta a la ubicación de tu imagen */
+            background-size: cover; /* Ajusta el tamaño de la imagen para cubrir todo el fondo */
+        }
+
+    </style>
+
+<style>
+        .navbar-nav .nav-link {
+            margin-right: 100px; /* Agrega espacio entre los elementos */
+        }
+    </style>
+</head>
+
+<body>
+     <!-- Menú de Navegación -->
+     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">SGFS</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">INICIO <span class="sr-only"></span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">FUNCIONES</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ACERCA DE</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">AYUDA</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <h1> Bienvenidos</h1>
+
+    
+</body>
+</html>
